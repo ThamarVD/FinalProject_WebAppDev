4a63540993caadfd9ebf33bcf2a0836fc6e7fbee
Vinay Thamara <thamarvd@mail.uc.edu>
2024-11-25 00:14:43 -0500

    Fix Connection String

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     options =>
-        options.UseMySQL(builder.Configuration.GetConnectionString("ConnectionString"))
+        options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 var app = builder.Build();
