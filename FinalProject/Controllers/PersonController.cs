3a79f94240d71de618f3dd7816aee06f37dffe17
Vinay Thamara <thamarvd@mail.uc.edu>
2024-12-01 15:17:30 -0500

    Add Person Page

--- FinalProject/Controllers/PersonController.cs
+++ FinalProject/Controllers/PersonController.cs
@@ -8,18 +8,53 @@ public class PersonController(ApplicationDbContext context) : Controller
 {
     private readonly ApplicationDbContext _context = context;
     
-    public IActionResult View(int id)
+    public ViewResult View(int id)
     {
         var person = _context.People
             .Include(p => p.Hobbies)
             .Include(p => p.Degree)
             .FirstOrDefault(p => p.Id == id);
         
-        if (person == null)
-        {
-            return NotFound();
-        }
-        
         return View(person);
+    }
+    
+    [HttpGet]
+    public ViewResult Add()
+    {
+        var degrees = _context.Degrees.ToList();
+        ViewBag.Degrees = degrees;
+        
+        var hobbies = _context.Hobbies.ToList();
+        ViewBag.Hobbies = hobbies;
+        
+        return View(new Person()
+        {
+            Hobbies = new List<Hobby>()
+        });
+    }
+    
+    [HttpPost]
+    public IActionResult Add(Person person)
+    {
+        var degrees = _context.Degrees.ToList();
+        ViewBag.Degrees = degrees;
+        
+        if (ModelState.IsValid)
+        {
+            if (person.Hobbies.Any())
+            {
+                person.Hobbies = _context.Hobbies
+                    .Where(h => person.Hobbies.Select(ph => ph.Id).Contains(h.Id))
+                    .ToList();
+            }
+            
+            var addedPerson = _context.People.Add(person);
+            _context.SaveChanges();
+            
+            return Redirect($"/");
+        }
+
+        ModelState.AddModelError(String.Empty, "Please Correct All Errors");
+        return Add();
     }
 }
--- FinalProject/Models/Person.cs
+++ FinalProject/Models/Person.cs
@@ -1,14 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
 namespace FinalProject.Models;
 
 public class Person
 {
     public int Id { get; set; }
     
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
     public string Name { get; set; }
     
+    [Required(ErrorMessage = "About is required")]
+    [StringLength(500, ErrorMessage = "About cannot be longer than 500 characters")]
     public string About { get; set; }
     
-    public Degree Degree { get; set; }
+    [Required(ErrorMessage = "Please select a degree")]
+    [Range(1, int.MaxValue, ErrorMessage = "Please select a valid degree")]
+    public int DegreeId { get; set; }
+    public Degree? Degree { get; set; }
     
-    public List<Hobby> Hobbies { get; set; }
+    public List<Hobby>? Hobbies { get; set; } = new List<Hobby>();
 }
